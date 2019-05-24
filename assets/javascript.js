2e1e6906869c82336a7b184f3ef4397dfc184ce3
Arlen Cornejo <arlen.rose.cr@gmail.com>
2019-05-23 19:27:47 -0700

    First Commit

---
+++ assets/javascript.js
@@ -0,0 +1,6 @@
+$("button").on("click", function () {
+    console.log("you´ve been clicked!", $("#train-name").val())
+    console.log("you´ve been clicked!", $("#destination").val())
+    console.log("you´ve been clicked!", $("#startDate").val())
+    console.log("you´ve been clicked!", $("#frequency").val())
+  })
